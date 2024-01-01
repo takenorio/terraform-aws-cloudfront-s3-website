3030fcd9b47dc9b6b9cc1c9a9788ac41aaa8c0f3
OYA Takenori <t@kenor.io>
2024-01-01 20:41:11 +0900

    refactor(setup): init backend and AWS providers (#29)
    Refactored the Terraform configuration to establish a S3 backend and
    updated AWS provider settings, including a regional alias.

---
+++ examples/complete/backend.tf
@@ -0,0 +1,7 @@
+#terraform {
+#  backend "s3" {
+#    bucket = "your-bucket-name"
+#    key    = "your-tfstate-file-name"
+#    region = "your-region"
+#  }
+#}
---
+++ examples/complete/providers.tf
@@ -0,0 +1,8 @@
+#provider "aws" {
+#  region = "your-region"
+#}
+
+#provider "aws" {
+#  alias  = "us-east-1"
+#  region = "us-east-1"
+#}
