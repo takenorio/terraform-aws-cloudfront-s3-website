b408fae53742e784284f6ba529cd6a032a21dd95
OYA Takenori <t@kenor.io>
2024-01-01 22:34:30 +0900

    feat(s3): add redirect bucket and update naming (#30)
    * feat(s3): add redirect bucket and update naming
    
    Introduced a dedicated S3 bucket to handle domain redirections.
    Also refined the naming of existing buckets for better clarity.
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/redirection-bucket/variables.tf
+++ modules/redirection-bucket/variables.tf
@@ -1,14 +1,3 @@
-variable "cloudfront_distribution_comment" {
-  description = "A comment to describe the CloudFront distribution."
-  type        = string
-  default     = "static website"
-
-  validation {
-    condition     = length(var.cloudfront_distribution_comment) <= 128
-    error_message = "The total length cannot exceed 128 characters."
-  }
-}
-
 variable "domain_name" {
   description = "The domain name to be managed by Route53."
   type        = string
@@ -60,7 +49,7 @@ variable "logging_bucket_name" {
 }
 
 variable "redirection_bucket_name" {
-  description = "Name of the S3 bucket for storing redirection content. Must be globally unique."
+  description = "Name of the S3 bucket for storing website content. Must be globally unique."
   type        = string
 
   validation {
@@ -95,46 +84,6 @@ variable "redirection_bucket_name" {
 
   validation {
     condition     = !endswith(var.redirection_bucket_name, "-s3alias") && !endswith(var.redirection_bucket_name, "--ol-s3")
-    error_message = "Bucket names must not end with the suffix `-s3alias` and the suffix `--ol-s3`."
-  }
-}
-
-variable "website_bucket_name" {
-  description = "Name of the S3 bucket for storing website content. Must be globally unique."
-  type        = string
-
-  validation {
-    condition     = length(var.website_bucket_name) >= 3 && length(var.website_bucket_name) <= 63
-    error_message = "Bucket names must be between 3 (min) and 63 (max) characters long."
-  }
-
-  validation {
-    condition     = can(regex("^[a-z0-9-.]+$", var.website_bucket_name))
-    error_message = "Bucket names can consist only of lowercase letters, numbers, dots (.), and hyphens (-)."
-  }
-
-  validation {
-    condition     = can(regex("^[a-z0-9]", var.website_bucket_name)) && can(regex("[a-z0-9]$", var.website_bucket_name))
-    error_message = "Bucket names must begin and end with a letter or number."
-  }
-
-  validation {
-    condition     = !can(regex("[.]{2}", var.website_bucket_name))
-    error_message = "Bucket names must not contain two adjacent periods."
-  }
-
-  validation {
-    condition     = !can(regex("^(\\d+\\.\\d+\\.\\d+\\.\\d+)$", var.website_bucket_name))
-    error_message = "Bucket names must not be formatted as an IP address."
-  }
-
-  validation {
-    condition     = !startswith(var.website_bucket_name, "xn--") && !startswith(var.website_bucket_name, "sthree-")
-    error_message = "Bucket names must not start with the prefix `xn--` and the prefix `sthree-`."
-  }
-
-  validation {
-    condition     = !endswith(var.website_bucket_name, "-s3alias") && !endswith(var.website_bucket_name, "--ol-s3")
     error_message = "Bucket names must not end with the suffix `-s3alias` and the suffix `--ol-s3`."
   }
 }
