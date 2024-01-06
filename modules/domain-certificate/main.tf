9891651d68f140e4ce1b1a28b06c0662ee2e9900
OYA Takenori <t@kenor.io>
2024-01-06 16:04:28 +0900

    refactor(outputs): enhance clarity output names (#38)
    * refactor(outputs): enhance clarity output names
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/domain-certificate/main.tf
+++ modules/domain-certificate/main.tf
@@ -19,10 +19,10 @@ resource "aws_acm_certificate_validation" "this" {
   provider = aws.us-east-1
 
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn if record.name == var.domain_name]
+  validation_record_fqdns = [for record in aws_route53_record.certificate : record.fqdn if record.name == var.domain_name]
 }
 
-resource "aws_route53_record" "this" {
+resource "aws_route53_record" "certificate" {
   for_each = {
     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
